4e4f5e8fbbc263b18d042b1d26d8242f7b6d88ee
danielpdaniel <artofjquinn@gmail.com>
2022-08-22 14:38:49 -0400

    commit tree e5fc4261a73821eed36bac6a7be592424e7119fd

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+   pText = document.querySelector("p#text").textContent = "This is really cool!"
+});
+console.log("rest of index.js file loaded here!");
